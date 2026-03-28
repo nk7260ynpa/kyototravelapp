ec7f7da520569e85dfad611a5f310526c71b6e91
nk7260ynpa <nk7260ynpa@gmail.com>
2026-03-28 20:14:04 +0800

    Initial Commit

---
+++ kyototravelapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kyototravelapp
+//
+//  Created by Chen on 2026/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kyototravelapp/kyototravelappApp.swift
@@ -0,0 +1,17 @@
+//
+//  kyototravelappApp.swift
+//  kyototravelapp
+//
+//  Created by Chen on 2026/3/28.
+//
+
+import SwiftUI
+
+@main
+struct kyototravelappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
